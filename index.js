d2055e9083976daa3b47d0b968146b425af49749
André Victor <andre.victorueg@gmail.com>
2023-03-08 15:57:17 -0300

    Adicionado o projeto express e a api para os episodios

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+// Export the Express API
+module.exports = app
