1707c127a48fb03a50bded502bd4666e9812123d
André Victor <andre.victorueg@gmail.com>
2023-03-22 11:47:56 -0300

    Alteracao Vercel.json

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-// index.js
+const fs = require('fs');
+const path = require("path");
+
 const express = require('express')
 
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
+})
+
+app.get('/episodios', (req, res) => {
+  let rawdata = fs.readFileSync( path.resolve("database", "animes", "243_seiin_koukou_danshi_volleybu.json")) 
+  let json = JSON.parse(rawdata)
+  res.send(json)
 })
 
 app.get('/about', (req, res) => {
