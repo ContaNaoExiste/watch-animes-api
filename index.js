db63fb2405a635a3102673aa7a26e575fb7e087f
André Victor <andre.victorueg@gmail.com>
2023-03-22 11:59:30 -0300

    Alteracao Vercel.json

--- index.js
+++ index.js
@@ -2,21 +2,34 @@ const fs = require('fs');
 const path = require("path");
 
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
 
-app.get('/episodios', (req, res) => {
+app.get('/episodio/:id', (req, res) => {
   let rawdata = fs.readFileSync( path.resolve("database", "teste", "zatch_bell.json")) 
   let json = JSON.parse(rawdata)
   res.send(json)
 })
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
+app.get('/index/popular', (req, res) => {
+  let rawdata = fs.readFileSync( path.resolve("database", "teste", "zatch_bell.json")) 
+  let json = JSON.parse(rawdata)
+  let popular = {
+    animes:[
+      json,
+      json, 
+      json
+    ]
+  }
+  res.send(popular)
+})
+
+app.get('*', (req, res) => {
+  res.send({message: 'Endpoint não configurado!', error: 404})
 })
 
 // Export the Express API
